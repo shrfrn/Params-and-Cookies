864a46f031c57d80dcb93c0f4a14fe2fe227d93e
shrfrn <shrfrn@gmail.com>
2026-04-06 16:31:23 +0300

    Express 5.x + fallback route + cookie exmples

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ import cookieParser from 'cookie-parser'
 const app = express()
 app.use(cookieParser())
 
+// Demo end points
+
 app.get('/api/book', (req, res) => {
-    res.send(stringifyPayload(req, res))
+    res.send(stringifyPayload(req))
 })
 
 app.get('/api/book/:bookId', (req, res) => {
-    res.send(stringifyPayload(req, res))
+    res.send(stringifyPayload(req))
 })
 
 app.get('/api/book/:bookId/:chapterId', (req, res) => {
-    res.send(stringifyPayload(req, res))
+    res.send(stringifyPayload(req))
 })
 
 app.get('/api/set-cookie', (req, res) => {
@@ -21,44 +23,49 @@ app.get('/api/set-cookie', (req, res) => {
     const idx = getRandomInt(flavors.length)
 
     res.cookie('test-cookie', flavors[idx])
-    res.send(stringifyPayload(req, res))
+    res.send(stringifyPayload(req))
 })
 
 app.get('/api/clear-cookie', (req, res) => {
     res.clearCookie('test-cookie')
-    res.send(stringifyPayload(req, res))
+    res.send(stringifyPayload(req))
 })
 
 app.get('/api/counter-cookie', (req, res) => {
     let count = req.cookies.count || 0
     
     res.cookie('count', ++count)
-    res.send(stringifyPayload(req, res))
+    res.send(stringifyPayload(req))
 })
 
 app.get('/api/expiring-cookie', (req, res) => {
-    res.cookie('expiring-cookie', 'Hi', { maxAge: 3000 })
-    res.send(stringifyPayload(req, res))
+    const obj = { id: 100, txt: 'baba' }
+    res.cookie('expiring-cookie', JSON.stringify(obj), { maxAge: 3000 })
+    res.send(stringifyPayload(req))
 })
 
 app.get('/api/:id', (req, res) => {
-    res.send(stringifyPayload(req, res))
+    res.send(stringifyPayload(req))
 })
 
-app.get('*', (req, res) => {
-    res.send(stringifyPayload(req, res))
+// app.get('/favicon.ico', (req, res) => res.status(204).end())
+
+app.get('{*splat}', (req, res) => {
+    console.log('Hitting fallback route!')
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`)
+    res.send(`<h1 style="text-align: center">End point not found</h1>`)
 })
 
-function stringifyPayload(req, res){
-    const resStr = `
+// Utils
+
+function stringifyPayload(req){
+    return `
         <pre style="margin: 20px 5px">
             \nRoute: ${req.route.path}
             \nRoute Params: ${JSON.stringify(req.params, null, 4)}
             \nQuery String Params: ${JSON.stringify(req.query, null, 4)}
             \nCookies in request: ${JSON.stringify(req.cookies, null, 4)}
-        </pre>
-    `
-    return resStr
+        </pre>`
 }
 
 function getRandomInt(max) {
